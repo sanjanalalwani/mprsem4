f32dd43cdae3a4d7d5d574e094ec7ece2c0ec7b3
sanjanalalwani <610sanjana@gmail.com>
2023-03-04 23:10:40 +0530

    init

--- index.php
+++ index.php
@@ -52,17 +52,12 @@
     <div class="form-container">
 
     <form action="" method="post">
-        <h3>Register Now</h3>
+        <h3>Login</h3>
         <input type="text" name="name" required placeholder="enter your name">
         <input type="text" name="email" required placeholder="enter your email">
         <input type="password" name="password" required placeholder="enter password">
-        <input type="password" name="cpassword" required placeholder="confirm password">
-        <select name="user_type">
-            <option value="student">Student</option>
-            <option value="teacher">Teacher</option>
-        </select>
         <input type="submit" name="submit" value="register now" class="form-btn">
-        <p>Already have an account? <a href="index.php">Login</a></p>
+        <p>Don't have an account? <a href="register.php">Register</a></p>
     </form>
     </div>
           </div>
