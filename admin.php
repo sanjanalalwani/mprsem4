337a1b604d9c63eacaef235603f220ecc7c0fe25
sanjanalalwani <610sanjana@gmail.com>
2023-03-05 23:26:24 +0530

    init

--- admin.php
+++ admin.php
@@ -6,36 +6,3 @@ header('location:index.php');
 }
 ?>
 
-<html>
-<body>
-<link rel="stylesheet" href="popup.css">
-
-  <div class="center">
-    <input type="checkbox" id="click">
-    <label for="click" class="click-me">Choose</label>
-    <div class="content">
-      <div class="text">
-        Choose Class
-     </div>
-        <label for="click" id="temp">x</label>
-
-    <form>
-
-        <label for="username">FE</label>
-        <input type="radio" id="username">
-        <label for="username">SE</label>
-        <input type="radio" id="username">
-        <label for="username">TE</label>
-        <input type="radio"  id="username">
-        <label for="username">BE</label>
-        <input type="radio" id="username">
-        
-
-      <a href="div.html"> <button>Submit</button></a>
-
-    </form>
-      </div>
-    </div>
-  </div>
-</body>
-</html>
